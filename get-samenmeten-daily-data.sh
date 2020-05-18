680d89d54b80344bc6a30731cfdae92a35628a84
Phaze-III <xj.dropbox+github@gmail.com>
2020-05-18 15:39:44 +0200

    Allow multiple Sensors

--- get-samenmeten-daily-data.sh
+++ get-samenmeten-daily-data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-Sensor="${SAMENMETEN_SENSORID:-LTD_xxxxx}"  # Set this to your own Sensor ID
+Sensors="${SAMENMETEN_SENSORID:-LTD_xxxxx}"  # Set this to your own Sensor ID(s)
 DataDir="${SAMENMETEN_DATADIR:-.}"
 
 export INFLUXDB_DATABASE="${INFLUXDB_DATABASE:-samenmeten}"
@@ -47,42 +47,45 @@ fi
 APIURI='https://api-samenmeten.rivm.nl/v1.0'
 ODATAFilter="?\$filter=date%28phenomenonTime%29+${DateOperator:-eq}+date%28%27${Day}%27%29"
 
-DatastreamsTmpFile="${DataDir}/${Day}_Datastreams_${Sensor}${FileFill}_$(date +%s).csv"
-DatastreamsLink=$(curl -sS "${APIURI}/Things?\$filter=startswith(name,%27${Sensor}%27)" \
-                   | jq -r '.value[]."Datastreams@iot.navigationLink"')
-
-curl -sS "${DatastreamsLink}" \
-   | jq -r ' .value[] | [.description, ."@iot.id", ."Observations@iot.navigationLink"] | @csv' \
-   > "${DatastreamsTmpFile}"
-
-IFS=,
-cat ${DatastreamsTmpFile} | tr -d '<>|"' | \
-while read Description StreamID ObservationLink
+for Sensor in ${Sensors}
 do
-   Formula="${Description##${Sensor}-?-}"
-   APIEndPoint="Datastreams(${DatastreamID})/Observations"
-   Page=1
-   APICall="${ObservationLink}${ODATAFilter}"
-
-   while [ -n "${APICall}" ]
+   DatastreamsTmpFile="${DataDir}/${Day}_Datastreams_${Sensor}${FileFill}_$(date +%s).csv"
+   DatastreamsLink=$(curl -sS "${APIURI}/Things?\$filter=startswith(name,%27${Sensor}%27)" \
+                      | jq -r '.value[]."Datastreams@iot.navigationLink"')
+   
+   curl -sS "${DatastreamsLink}" \
+      | jq -r ' .value[] | [.description, ."@iot.id", ."Observations@iot.navigationLink"] | @csv' \
+      > "${DatastreamsTmpFile}"
+   
+   IFS=,
+   cat ${DatastreamsTmpFile} | tr -d '<>|"' | \
+   while read Description StreamID ObservationLink
    do
-      jsonFile="${DataDir}/${Day}_${Description}${FileFill}_p$(printf %03d ${Page}).json"
-      csvFile="${DataDir}/${Day}_${Description}${FileFill}_p$(printf %03d ${Page}).csv"
-      curl -sS --location "${APICall}" > "${jsonFile}"
-      NextLink=$(jq -r '."@iot.nextLink" // "" ' "${jsonFile}")
-      APICall=${NextLink}
-      Page=$((${Page} + 1))
-      case ${Formula} in
-      ${Store})
-         cat "${jsonFile}" \
-           | jq -r '.value[]|flatten|@csv' \
-           | awk -F, -v OFS=, -v Sensor=${Sensor} -v Formula=${Formula} \
-                '{if (length($4) > 0) { gsub(/.000Z/, "+00:00", $3 ) ;print Sensor,$4, $3, toupper(Formula) }}' \
-           > "${csvFile}"
-         cat "${csvFile}" | rivm-to_line_protocol.py | to_influx_db.sh 
-         ;;
-      esac
+      Formula="${Description##${Sensor}-?-}"
+      APIEndPoint="Datastreams(${DatastreamID})/Observations"
+      Page=1
+      APICall="${ObservationLink}${ODATAFilter}"
+   
+      while [ -n "${APICall}" ]
+      do
+         jsonFile="${DataDir}/${Day}_${Description}${FileFill}_p$(printf %03d ${Page}).json"
+         csvFile="${DataDir}/${Day}_${Description}${FileFill}_p$(printf %03d ${Page}).csv"
+         curl -sS --location "${APICall}" > "${jsonFile}"
+         NextLink=$(jq -r '."@iot.nextLink" // "" ' "${jsonFile}")
+         APICall=${NextLink}
+         Page=$((${Page} + 1))
+         case ${Formula} in
+         ${Store})
+            cat "${jsonFile}" \
+              | jq -r '.value[]|flatten|@csv' \
+              | awk -F, -v OFS=, -v Sensor=${Sensor} -v Formula=${Formula} \
+                   '{if (length($4) > 0) { gsub(/.000Z/, "+00:00", $3 ) ;print Sensor,$4, $3, toupper(Formula) }}' \
+              > "${csvFile}"
+            cat "${csvFile}" | rivm-to_line_protocol.py | to_influx_db.sh 
+            ;;
+         esac
+      done
    done
+   
+   rm -f ${DatastreamsTmpFile}
 done
-
-rm -f ${DatastreamsTmpFile}
